bac4fd575517a1e3981cff07c0f607aee497e7b9
TashChetty <natasha.chetty2@gmail.com>
2023-09-29 07:34:48 +0200

    lesson-1

---
+++ lesson-1/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react'; //library for creating views
+import ReactDOM from 'react-dom/client'; //library used to render the UI in browser
+import './index.css'; //impotr css
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
