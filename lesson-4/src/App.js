e0cde1614d890e707c1a7ae09584ff147610fc06
TashChetty <natasha.chetty2@gmail.com>
2023-09-29 07:53:49 +0200

    lesson-4

---
+++ lesson-4/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Products from './Products';
+
+
+class App extends Component {
+  render() {
+      return (
+      <div>
+        <h5>Natasha's List</h5>
+        <Products />
+         
+      </div>
+    );
+  }
+}
+export default App;
