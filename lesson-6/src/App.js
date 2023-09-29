3906177b5c28ac9880fa1f28ba7d29d9240d2cff
TashChetty <natasha.chetty2@gmail.com>
2023-09-29 07:57:27 +0200

    lesson-6

---
+++ lesson-6/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Products from './Products';
+import UserForm from './UserForm';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <UserForm/>
+         
+      </div>
+    );
+  }
+}
+export default App;
